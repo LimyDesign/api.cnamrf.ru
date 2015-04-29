7c2a97350b07d9bf7c631b75cec3f0cc91031bb0
Arsen Bespalov <me@arsen.pw>
2015-04-29 14:02:18 +0800

    update api

--- index.php
+++ index.php
@@ -124,7 +124,10 @@ function getName($number) {
 							'what' => $number,
 							'where' => $city));
 						$dublgis = json_decode(file_get_contents($url.$uri));
-						$json_return = array('error' => 0, 'name' => $dublgis->result[0]->name, 'translit' => '');
+						$name = $dublgis->result[0]->name;
+						if ($name) {
+							$json_return = array('error' => 0, 'name' => $name, 'translit' => rus2translit($name));
+						}
 					}
 				}
 			} else {
@@ -141,4 +144,34 @@ function getName($number) {
 function defaultResult() {
 	return json_encode(array('error' => 1, 'message' => 'Failed requests to the API interface.'));
 }
+
+function rus2translit($string) {
+    $converter = array(
+        'а' => 'a',   'б' => 'b',   'в' => 'v',
+        'г' => 'g',   'д' => 'd',   'е' => 'e',
+        'ё' => 'e',   'ж' => 'zh',  'з' => 'z',
+        'и' => 'i',   'й' => 'y',   'к' => 'k',
+        'л' => 'l',   'м' => 'm',   'н' => 'n',
+        'о' => 'o',   'п' => 'p',   'р' => 'r',
+        'с' => 's',   'т' => 't',   'у' => 'u',
+        'ф' => 'f',   'х' => 'h',   'ц' => 'c',
+        'ч' => 'ch',  'ш' => 'sh',  'щ' => 'sch',
+        'ь' => '',    'ы' => 'y',   'ъ' => '',
+        'э' => 'e',   'ю' => 'yu',  'я' => 'ya',
+        
+        'А' => 'A',   'Б' => 'B',   'В' => 'V',
+        'Г' => 'G',   'Д' => 'D',   'Е' => 'E',
+        'Ё' => 'E',   'Ж' => 'Zh',  'З' => 'Z',
+        'И' => 'I',   'Й' => 'Y',   'К' => 'K',
+        'Л' => 'L',   'М' => 'M',   'Н' => 'N',
+        'О' => 'O',   'П' => 'P',   'Р' => 'R',
+        'С' => 'S',   'Т' => 'T',   'У' => 'U',
+        'Ф' => 'F',   'Х' => 'H',   'Ц' => 'C',
+        'Ч' => 'Ch',  'Ш' => 'Sh',  'Щ' => 'Sch',
+        'Ь' => '',    'Ы' => 'Y',   'Ъ' => '',
+        'Э' => 'E',   'Ю' => 'Yu',  'Я' => 'Ya',
+    );
+    return strtr($string, $converter);
+}
+
 ?>

4350395b57230576cf4a72719bde5862f237c4f8
Arsen Bespalov <me@arsen.pw>
2015-04-29 15:07:30 +0800

    update api

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ function getName($number) {
 		if ($conf['db']['type'] == 'postgres')
 		{
 			$db_err_message = array('error' => 100, 'message' => 'Unable to connect to database. Please send message to support@cnamrf.ru about this error.');
-			$db = pg_connect('dbname=asd') or die(json_encode($db_err_message));
+			$db = pg_connect('dbname='.$conf['db']['database']) or die(json_encode($db_err_message));
 			$query = "select users.id, users.qty, tariff.price from users left join tariff on users.tariffid = tariff.id where apikey = '{$uAPIKey}'";
 			$result = pg_query($query);
 			$uid = pg_fetch_result($result, 0, 'id');

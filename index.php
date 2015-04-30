b4d8885009a281289f434edab02082fdd1d648e6
Arsen Bespalov <me@arsen.pw>
2015-04-30 12:43:07 +0800

    update get2GisCities() function

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ function get2GisCities()
 						$query .= "'{$city->name}',";
 					}
 					$query = substr($query, 0, -1);
-					$query = "select insertCities({$query})";
+					$query = "select insertCities(array[{$query}])";
 					die($query);
 					pg_query($query);
 					return json_encode(array('error' => 0, 'total' => $total));

630703903eab0841db717e83c54b36322f026d90
Arsen Bespalov <me@arsen.pw>
2015-05-21 12:42:48 +0800

    update api

--- index.php
+++ index.php
@@ -201,25 +201,29 @@ function get2GisRubrics($city_id)
 					'where' => $city_name,
 					'show_children' => '1'));
 				$dublgis = json_decode(file_get_contents($url.$uri));
-				$query = "DELETE FROM rubrics;\n";
-				file_put_contents('rubrics.sql', $query, LOCK_EX);
+				$query = "-- Удаляем все данные из таблицы связанные именно с этим городом"."\n";
+				$query.= "DELETE FROM rubrics WHERE city_id = {$city_id};"."\n";
+				file_put_contents("rubrics-{$city_id}.sql", $query, LOCK_EX);
 				foreach ($dublgis->result as $result) {
 					$id_parent = $result->id;
 					$name_parent = pg_escape_string($result->name);
 					$alias_parent = pg_escape_string($result->alias);
-					$query = "\n"."INSERT INTO rubrics (id, name, alias, city_id) VALUES ({$id_parent}, '{$name_parent}', '{$alias_parent}', {$city_id});"."\n";
-					file_put_contents('rubrics.sql', $query, LOCK_EX | FILE_APPEND);
+					$query = "\n"."-- Добавляем родительскую рубрику"."\n";
+					$query.= "INSERT INTO rubrics (id, name, alias, city_id) VALUES ({$id_parent}, '{$name_parent}', '{$alias_parent}', {$city_id});"."\n";
+					file_put_contents("rubrics-{$city_id}.sql", $query, LOCK_EX | FILE_APPEND);
 					if ($result->children) {
+						$query = "-- Добавляем дочерние рубрики"."\n";
+						file_put_contents("rubrics-{$city_id}.sql", $query, LOCK_EX | FILE_APPEND);
 						foreach ($result->children as $children) {
 							$id = $children->id;
 							$name = pg_escape_string($children->name);
 							$alias = pg_escape_string($children->alias);
 							$query = "INSERT INTO rubrics (id, name, alias, parent_id, city_id) VALUES ({$id}, '{$name}', '{$alias}', {$id_parent}, {$city_id});"."\n";
-							file_put_contents('rubrics.sql', $query, LOCK_EX | FILE_APPEND);
+							file_put_contents("rubrics-{$city_id}.sql", $query, LOCK_EX | FILE_APPEND);
 						}
 					}
 				}
-				$json_return = array('error' => '0', 'sql_dump_created' => 'rubrics.sql');
+				$json_return = array('error' => '0', 'sql_dump_created' => "rubrics-{$city_id}.sql");
 			} else {
 				return json_encode(array('error' => '6', 'message' => 'Access deny.'));
 			}
